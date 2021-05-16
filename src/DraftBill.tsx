3d93655e09392e2153cdd26213bdb888f97dab3c
Joel Stauffer <altosax13@gmail.com>
2021-05-15 22:46:16 -0400

    feat: fleshed out more of the draft bill page

--- src/DraftBill.tsx
+++ src/DraftBill.tsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
-import { Bill, Client } from '../types/calls';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { AppColorStyles, AppFontStyles } from '../styles/default';
+import { Bill, Call, Client } from '../types/calls';
 import { DraftBillProps } from '../types/routes';
 import AppButton from './control/AppButton';
+import MultiActionButton from './control/MultiActionButton';
+import DoubleTextLayout from './custom-control/DoubleTextLayout';
 import useBills from './hooks/useBills';
 import useClients from './hooks/useClients';
+import { formatHoursMinutesSeconds, formatTimestamp } from './service/formatter';
 
 const DraftBill = ({ navigation, route }: DraftBillProps) => {
 
@@ -24,29 +28,52 @@ const DraftBill = ({ navigation, route }: DraftBillProps) => {
     loadBill();
   }, [])
 
-  const onGoToCallLinkedToClient = () => navigation.push('CallLinkedToClient');
+  const getIcon = (item: Call): string =>
+    item.isBilled ? 'check-square' : 'square';
+
+  const toggleCallBilledStatus = pk => { };
+
+  const onGoToCallLinkedToClient = pk => navigation.push('CallLinkedToClient');
   const onGoToBill = () => navigation.navigate('Bill');
 
   const calls = bill === null ? [] : bill.calls;
   const clientName = client === null ? "" : client.name;
+  const callDurationSum = calls.reduce((prev, cur) => cur.duration + prev, 0);
+
+  const earliestCall = calls.reduce((prev, cur) => parseInt(cur.timestamp) > parseInt(prev.timestamp) ? prev : cur, calls[0]);
+  const earliestCallDate = calls.length > 0
+    ?  formatTimestamp(earliestCall.timestamp)
+    : "";
+
+  const latestCall = calls.reduce((prev, cur) => parseInt(cur.timestamp) < parseInt(prev.timestamp) ? prev : cur, calls[0]);
+  const latestCallDate = calls.length > 0
+    ?  formatTimestamp(latestCall.timestamp)
+    : "";
 
   return (
     <View style={styles.container} >
-      <Text>Draft Bill</Text>
-      <Text>Client: {clientName}</Text>
-      <Text>Calls</Text>
+      <Text style={styles.header}>{clientName}</Text>
+      <DoubleTextLayout label="Total Duration:" content={formatHoursMinutesSeconds(callDurationSum)} />
+      <DoubleTextLayout label="Number of Calls:" content={calls.length.toString()} />
+      <DoubleTextLayout label="Earliest Call:" content={earliestCallDate} />
+      <DoubleTextLayout label="Latest Call:" content={latestCallDate} />
+      <Text style={styles.header}>Calls</Text>
       <FlatList
         data={calls}
         keyExtractor={c => c.pk.toString()}
         renderItem={
           ({ item }) => (
-            <AppButton
-              onPress={onGoToCallLinkedToClient}
-              title={item.contactNotes + ' ' + item.callReason} />
+            <MultiActionButton
+              mainTitle={item.contactNotes + ' ' + item.callReason}
+              onPressMainAction={() => onGoToCallLinkedToClient(item.pk.toString())}
+              onPressSecondaryAction={() => toggleCallBilledStatus(item.pk.toString())}
+              secondaryTitle="invoice call"
+              secondarySymbol={getIcon(item)} />
           )
         }
       />
-      <Button onPress={onGoToBill} title="Invoice Bill"></Button>
+      <AppButton onPress={onGoToBill} title="Finalize Bill" />
+      <View style={styles.spacer}></View>
     </View>
   );
 };
@@ -54,10 +81,20 @@ const DraftBill = ({ navigation, route }: DraftBillProps) => {
 export default DraftBill;
 
 const styles = StyleSheet.create({
+  spacer: {
+    padding: 10 
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    color: AppColorStyles.text,
+    textAlign: 'center',
+    fontSize: AppFontStyles.titleSize,
+    marginBottom: 10,
+    marginTop: 10,
   },
 });
