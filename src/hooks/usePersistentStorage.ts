1a1fd53672fcfb7305560bad731a3f1a7b9d845d
Joel Stauffer <altosax13@gmail.com>
2021-11-10 22:20:07 -0500

    wip: solid stab at good encryption level

--- src/hooks/usePersistentStorage.ts
+++ src/hooks/usePersistentStorage.ts
@@ -1,25 +1,14 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import Realm from 'realm';
 import { Schema_2 } from '../models/Schema';
+import { getRealmEncryptionKey } from '../service/keyGenerator';
 
 const usePersistentStorage = () => {
-
-    const keyStoreValue = 'encrypt-key';
-
-    const getKey = async () => {
-        let key = await AsyncStorage.getItem(keyStoreValue);
-        let encrypt: Uint8Array = new Uint8Array(key ? parseInt(key) : Date.now());
-        if (!key)
-            await AsyncStorage.setItem(keyStoreValue, encrypt.toString());
-        return encrypt;
-    }
-
     const getRealm = async () => {
         return await Realm.open({
             schema: Schema_2.schema,
             schemaVersion: Schema_2.version,
             path: "bill-me-repositiory",
-            encryptionKey: await getKey()
+            encryptionKey: await getRealmEncryptionKey()
         });
     }
 
