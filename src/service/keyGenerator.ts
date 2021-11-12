937d440b0d81e4ac1169eedc7a6d1b721fd61b4c
Joel Stauffer <altosax13@gmail.com>
2021-11-11 20:25:53 -0500

    feat: encryption complete

--- src/service/keyGenerator.ts
+++ src/service/keyGenerator.ts
@@ -1,11 +1,12 @@
 import { generateSecureRandom } from 'react-native-securerandom';
 import * as Keychain from 'react-native-keychain';
+import { Buffer } from 'buffer';
 
 const convertKeyStringToBuffer: (key: string) => Uint8Array = (key: string) =>
-    Uint8Array.from(Array.from(key).map(letter => letter.charCodeAt(0)));
+    new Uint8Array(Buffer.from(key, 'base64').buffer);
 
 const convertKeyBufferToString: (rawKey: Uint8Array) => string = (rawKey: Uint8Array) =>
-    Array.from(rawKey).map(byte => String.fromCharCode(byte)).join('');
+    Buffer.from(rawKey).toString('base64');
 
 const getRealmEncryptionKey: () => Promise<Uint8Array> = async () => {
     const username = 'dont-care';
